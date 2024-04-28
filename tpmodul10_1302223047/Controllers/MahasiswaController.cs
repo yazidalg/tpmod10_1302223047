72217321c759d1a899c100637d83e9622aefd111
yazidalg <yazid.al2418@gmail.com>
2024-04-29 01:20:46 +0700

    first blood

---
+++ tpmodul10_1302223047/Controllers/MahasiswaController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace tpmodul10_1302223047.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+
+	public class MahasiswaController : ControllerBase
+	{
+		private static List<Model.Mahasiswa> listOfMahasiswa = new List<Model.Mahasiswa>
+		{
+			new Model.Mahasiswa("Yazid Al Ghozali", "1302223047"),
+			new Model.Mahasiswa("Meira Hafshah", "13022223048"),
+		};
+
+		[HttpGet]
+		public IEnumerable<Model.Mahasiswa> Get()
+		{
+			return listOfMahasiswa;
+		}
+
+		[HttpGet("{id}")]
+		public Model.Mahasiswa Get(int id)
+		{
+			return listOfMahasiswa[id];
+		}
+
+		[HttpPost]
+		public void Post([FromBody] Model.Mahasiswa value)
+		{
+			listOfMahasiswa.Add(value);
+		}
+
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] Model.Mahasiswa value)
+		{
+			listOfMahasiswa[id] = value;
+		}
+
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+			listOfMahasiswa.RemoveAt(id);
+		}
+	}
+}
+
---
+++ tpmodul10_1302223047/Model/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace tpmodul10_1302223047.Model
+{
+	public class Mahasiswa
+	{
+		public string nama { get; set; }
+		public string nim { get; set; }
+	
+		public Mahasiswa(string nama, string nim)
+		{
+			this.nama = nama;
+			this.nim = nim;
+		}
+	}
+}
+
